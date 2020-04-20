f1199ee58c748d2296dd859c38133fe9075aa1df
Mate Balo Bordas <mate.balob@gmail.com>
2020-04-20 20:22:41 +0100

    fix function exporting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import { mapFieldsFromApi } from './mapFromApi.js';
 import { mapFieldsForApi } from './mapForApi.js';
 
-export {
-    mapFieldsFromApi,
-    mapFieldsForApi,
-};
+(function (global) {
+    const JSORMLib ={
+        mapFieldsFromApi,
+        mapFieldsForApi,
+    };
+
+    if (typeof define === 'function' && define.amd) {
+        define(function () { return JSORMLib; });
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = JSORMLib;
+    } else {
+        global.JSORMLib = JSORMLib;
+    }
+})(this);
